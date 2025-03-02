078fe84ba19d5184009562f77c8f35110755323b
Luis Castillo <luiseduardocastilloruvalcaba@gmail.com>
2025-03-01 21:49:34 -0600

    Add initial project configuration files for IntelliJ IDEA 2

---
+++ Videogame/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello World");
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
+
